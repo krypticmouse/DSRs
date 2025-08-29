4afb83092fa1fb9e4103e9e7022ae162545ee966
Herumb Shandilya <43719685+krypticmouse@users.noreply.github.com>
2025-08-29 00:23:06 -0700

    update macros

--- crates/dspy-rs/src/lib.rs
+++ crates/dspy-rs/src/lib.rs
@@ -3,111 +3,73 @@ pub mod core;
 pub mod data;
 pub mod evaluate;
 pub mod predictors;
+pub mod utils;
 
-// pub use dsrs_macros::*;
+pub use core::*;
+pub use data::*;
+pub use evaluate::*;
+pub use predictors::*;
+pub use utils::*;
 
-// #[macro_export]
-// macro_rules! sign {
-//     // Example Usage: signature! {
-//     //     question: String, random: bool -> answer: String
-//     // }
-//     //
-//     // Example Output:
-//     //
-//     // #[derive(Signature)]
-//     // struct InlineSignature {
-//     //     question: In<String>,
-//     //     random: In<bool>,
-//     //     answer: Out<String>,
-//     // }
-//     //
-//     // InlineSignature::new()
+pub use dsrs_macros::*;
 
-//     // Pattern: input fields -> output fields
-//     { ($($input_name:ident : $input_type:ty),* $(,)?) -> $($output_name:ident : $output_type:ty),* $(,)? } => {{
-//         use dspy_rs::internal::{MetaField, MetaSignature};
-//         use indexmap::IndexMap;
+#[macro_export]
+macro_rules! sign {
+    // Example Usage: signature! {
+    //     question: String, random: bool -> answer: String
+    // }
+    //
+    // Example Output:
+    //
+    // #[derive(Signature)]
+    // struct InlineSignature {
+    //     question: In<String>,
+    //     random: In<bool>,
+    //     answer: Out<String>,
+    // }
+    //
+    // InlineSignature::new()
 
-//         let mut input_fields = IndexMap::new();
-//         let mut output_fields = IndexMap::new();
+    // Pattern: input fields -> output fields
+    { ($($input_name:ident : $input_type:ty),* $(,)?) -> $($output_name:ident : $output_type:ty),* $(,)? } => {{
+        use dspy_rs::core::MetaSignature;
+        use serde_json::Value;
+        
+        let mut input_fields = serde_json::Map::new();
+        let mut output_fields = serde_json::Map::new();
 
-//         // Add input fields
-//         $(
-//             let json_value = serde_json::to_value(&schemars::schema_for!($input_type)).unwrap();
+        #[Signature]
+        struct InlineSignature {
+            $(
+                #[input]
+                $input_name: $input_type,
+            )*
+            $(
+                #[output]
+                $output_name: $output_type,
+            )*
+        }
+        
+        InlineSignature::new()
+    }};
+}
 
-//             let schema = if let Some(properties) = json_value.as_object()
-//                 .and_then(|obj| obj.get("properties"))
-//                 .and_then(|props| props.as_object()) {
-//                 serde_json::to_string(&properties).unwrap_or_else(|_| "".to_string())
-//             } else {
-//                 "".to_string()
-//             };
+#[macro_export]
+macro_rules! example {
+    // Pattern: { "key": "value", ... }
+    { $($key:literal : $value:expr),* $(,)? } => {{
+        use std::collections::HashMap;
+        use dspy_rs::data::example::Example;
 
-//             input_fields.insert(
-//                 stringify!($input_name).to_string(),
-//                 MetaField {
-//                     desc: String::new(),
-//                     schema: schema,
-//                     data_type: stringify!($input_type).to_string(),
-//                     __dsrs_field_type: "Input".to_string(),
-//                 }
-//             );
-//         )*
+        let mut fields = HashMap::new();
+        $(
+            fields.insert($key.to_string(), $value.to_string());
+        )*
 
-//         // Add output fields
-//         $(
-//             let json_value = serde_json::to_value(&schemars::schema_for!($output_type)).unwrap();
-
-//             let schema = if let Some(properties) = json_value.as_object()
-//                 .and_then(|obj| obj.get("properties"))
-//                 .and_then(|props| props.as_object()) {
-//                 serde_json::to_string(&properties).unwrap_or_else(|_| "".to_string())
-//             } else {
-//                 "".to_string()
-//             };
-
-//             output_fields.insert(
-//                 stringify!($output_name).to_string(),
-//                 MetaField {
-//                     desc: String::new(),
-//                     schema: schema,
-//                     data_type: stringify!($output_type).to_string(),
-//                     __dsrs_field_type: "Output".to_string(),
-//                 }
-//             );
-//         )*
-
-//         // Generate instruction string
-//         let input_names: Vec<&str> = vec![$(stringify!($input_name)),*];
-//         let output_names: Vec<&str> = vec![$(stringify!($output_name)),*];
-
-//         let instruction = "".to_string();
-
-//         MetaSignature {
-//             name: "InlineSignature".to_string(),
-//             instruction,
-//             input_fields,
-//             output_fields,
-//         }
-//     }};
-// }
-
-// #[macro_export]
-// macro_rules! example {
-//     // Pattern: { "key": "value", ... }
-//     { $($key:literal : $value:expr),* $(,)? } => {{
-//         use std::collections::HashMap;
-//         use dspy_rs::data::example::Example;
-
-//         let mut fields = HashMap::new();
-//         $(
-//             fields.insert($key.to_string(), $value.to_string());
-//         )*
-
-//         Example::new(
-//             fields,
-//             vec![],
-//             vec![],
-//         )
-//     }};
-// }
+        Example::new(
+            fields,
+            vec![],
+            vec![],
+        )
+    }};
+}
