828f973d4235d8298bd499e165f9947920238495
Darin Kishore <darinkishore@protonmail.com>
2026-01-30 13:14:00 -0800

    dsrs-n9u.41: add default signature templates

--- crates/dspy-rs/src/core/signature/compiled.rs
+++ crates/dspy-rs/src/core/signature/compiled.rs
@@ -7,6 +7,31 @@ use crate::baml_bridge::prompt::{PromptWorld, RenderError};
 
 use super::{SigMeta, Signature};
 
+/// Default system template - describes input/output fields.
+pub const DEFAULT_SYSTEM_TEMPLATE: &str = r#"
+Your input fields are:
+{% for f in sig.inputs -%}
+- {{ f.llm_name }}: {{ f.type_name }}
+{% endfor %}
+
+Your output fields are:
+{% for f in sig.outputs -%}
+- {{ f.llm_name }}: {{ f.type_name }}
+{% if f.schema %}
+{{ f.schema }}
+{% endif %}
+{% endfor %}
+"#;
+
+/// Default user template - renders actual input values.
+pub const DEFAULT_USER_TEMPLATE: &str = r#"
+{% for f in sig.inputs %}
+[[ ## {{ f.llm_name }} ## ]]
+{{ inputs[f.rust_name] }}
+
+{% endfor %}
+"#;
+
 /// A compiled signature ready for prompt rendering.
 pub struct CompiledSignature<S: Signature> {
     /// The prompt world with types and renderers.
@@ -24,6 +49,48 @@ pub struct CompiledSignature<S: Signature> {
 pub struct RenderedMessages {
     pub system: String,
     pub user: String,
+}
+
+pub fn register_default_templates(
+    world: &mut PromptWorld,
+) -> Result<(), Box<RenderError>> {
+    world
+        .jinja
+        .add_template_owned("sig::system", DEFAULT_SYSTEM_TEMPLATE.to_string())
+        .map_err(|err| {
+            Box::new(
+                RenderError::template_error(
+                    "<signature>",
+                    "signature",
+                    "default",
+                    "template",
+                    "sig::system",
+                    err.line().map(|line| (line, 0)),
+                    err.to_string(),
+                )
+                .with_cause(err),
+            )
+        })?;
+
+    world
+        .jinja
+        .add_template_owned("sig::user", DEFAULT_USER_TEMPLATE.to_string())
+        .map_err(|err| {
+            Box::new(
+                RenderError::template_error(
+                    "<signature>",
+                    "signature",
+                    "default",
+                    "template",
+                    "sig::user",
+                    err.line().map(|line| (line, 0)),
+                    err.to_string(),
+                )
+                .with_cause(err),
+            )
+        })?;
+
+    Ok(())
 }
 
 impl<S: Signature> CompiledSignature<S> {
